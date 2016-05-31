3de51f1fc9ebc65b153b22b0022e21ece4d5a0dd
Leigha <leigham314@gmail.com>
2016-05-31 14:59:56 -0400

    Learning my birth year

--- ruby_fundamentals1/exercise3.rb
+++ ruby_fundamentals1/exercise3.rb
@@ -1,5 +1,7 @@
 puts "what is your name?"
-
 name = gets.chomp
-
 puts "Hi #{name}!"
+
+puts "how many years have you been alive?"
+age = (gets.chomp).to_i
+puts "wow, you were born in #{2016 - age}!?"
