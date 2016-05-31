3f2f801d92528544d434e4e1c621d411e849c4f0
Leigha <leigham314@gmail.com>
2016-05-31 15:04:04 -0400

    Updated the date to now

--- ruby_fundamentals1/exercise3.rb
+++ ruby_fundamentals1/exercise3.rb
@@ -4,4 +4,4 @@ puts "Hi #{name}!"
 
 puts "how many years have you been alive?"
 age = (gets.chomp).to_i
-puts "wow, you were born in #{2016 - age}!?"
+puts "wow, you were born in #{Time.now.year - age}!?"
